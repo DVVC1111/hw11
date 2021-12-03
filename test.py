bb87060e7b7ff8f3fe5a2caa8adda0123996abd0
DVVC1111 <86997642+DVVC1111@users.noreply.github.com>
2021-12-03 13:27:54 +0700

    Phase 2

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import mysql_connect
 from datetime import datetime
+from prettytable import PrettyTable
 
 
 '''
@@ -19,7 +20,7 @@ if phone in result:
     print("Member found")
     
     
-    '''
+    
 today = datetime.today().strftime('%Y-%m-%d')
 mydb = mysql_connect.sql_connect()
 mycursor = mydb.cursor()
@@ -29,3 +30,27 @@ val = (10, 1, 1.0, today)
 mycursor.execute(sql, val)
 mydb.commit()
 print(mycursor.rowcount, " Sale Successful")
+'''
+"""
+start_date = input("Enter your start date (YYYY-MM-DD): ")
+end_date = input("Enter your end date (YYYY-MM-DD): ")
+
+
+mydb = mysql_connect.sql_connect()
+mycursor = mydb.cursor()
+sql = '''select sell.sell_id, customer.cus_firstname, customer.cus_lastname, coffee.coffee_name, coffee.coffee_price from sell 
+INNER JOIN customer on customer.cus_id = sell.cus_id
+INNER JOIN coffee on coffee.coffee_id = sell.coffee_id 
+WHERE sell_date BETWEEN %s and %s ''' 
+ 
+
+mycursor.execute(sql, (start_date, end_date))
+
+result = mycursor.fetchall()
+
+customer_information = PrettyTable()
+customer_information.field_names = ["Sell ID", "First Name", "Last Name", "Coffee", "Sell"]
+customer_information.add_rows(result) 
+
+print(customer_information.get_string())
+"""
